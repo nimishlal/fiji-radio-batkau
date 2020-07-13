9ddaf1d69d98d348f864494c5f6883c7fc827ad2
nimishlal <56611481+nimishlal@users.noreply.github.com>
2020-07-13 14:32:56 -0700

    update1.1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1 className="center">Fiji Radio</h1>
       <div>
+        <br></br>
+      <div className="text-center border">
         <h1>
           Radio Fiji 2
       </h1>
         <audio width="" height="" controls >
-      <source src="http://peridot.streamguys.com:7150/RFTwo" type="audio/mp3"/>
-      </audio>
-      <h1>Radio navtarang fiji</h1>
-      <audio controls>
-      <source src="http://stream.fijivillage.com/navtarang" type="audio/mp3"></source>
-      </audio>
-      <h1>Radio Sargam</h1>
-      <audio controls>
-      <source src="http://stream.fijivillage.com/radiosargam" type="audio/mp3"></source>
-      </audio>
+          <source src="http://peridot.streamguys.com:7150/RFTwo" type="audio/mp3" />
+        </audio>
       </div>
-      
+      <br></br>
+      <div className="text-center border">
+        <h1>Radio navtarang fiji</h1>
+        <audio controls>
+          <source src="http://stream.fijivillage.com/navtarang" type="audio/mp3"></source>
+        </audio>
+      </div>
+      <br></br>
+      <div className="text-center border">
+        <h1>Radio Sargam</h1>
+        <audio controls>
+          <source src="http://stream.fijivillage.com/radiosargam" type="audio/mp3"></source>
+        </audio>
+      </div>
+      </div>
+
       {/* <div>
              <h1>Radio Mirchi Fiji</h1>
       <video width="" height="" controls >
       <source src="http://peridot.streamguys.com:7150/Mirchi" type="audio/mpeg"/>
       </video>
       </div> */}
-      <footer className="colorFooter">Made By Chidoro Batkau V1.0 MixKarDo</footer>
+      <footer className="colorFooter text-center">Made By Chidoro Batkau V1.0 MixKarDo</footer>
     </div>
   );
 }
