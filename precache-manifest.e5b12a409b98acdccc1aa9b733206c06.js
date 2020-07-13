4befe2e74a3089208c853ed44d2c21c34f85a7fb
nimishlal <56611481+nimishlal@users.noreply.github.com>
2020-07-12 17:19:54 -0700

    Updates

---
+++ precache-manifest.e5b12a409b98acdccc1aa9b733206c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4689951a7fddc0540fc3fc2d1fd03c",
+    "url": "/fiji-radio-batkau/index.html"
+  },
+  {
+    "revision": "537de556387ee6c57302",
+    "url": "/fiji-radio-batkau/static/css/main.a09c16ca.chunk.css"
+  },
+  {
+    "revision": "068e6e002d3db151b646",
+    "url": "/fiji-radio-batkau/static/js/2.1c2cd649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fiji-radio-batkau/static/js/2.1c2cd649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537de556387ee6c57302",
+    "url": "/fiji-radio-batkau/static/js/main.6f7c11ea.chunk.js"
+  },
+  {
+    "revision": "c9fdcca83b0829c64198",
+    "url": "/fiji-radio-batkau/static/js/runtime-main.4001c5ab.js"
+  },
+  {
+    "revision": "3e5d793b30428e013d0a0911bd2d5c8c",
+    "url": "/fiji-radio-batkau/static/media/jyotish.3e5d793b.jpg"
+  }
+]);
