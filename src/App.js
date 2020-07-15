da6cc367a7374f05f0183d1b54bbe16efa06feeb
nimishlal <56611481+nimishlal@users.noreply.github.com>
2020-07-14 17:08:26 -0700

    update btn

--- src/App.js
+++ src/App.js
@@ -4,39 +4,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+
 function App() {
+
+  function refreshPage() {
+    window.location.reload(false)
+  }
   return (
-    
+
+
     <div className="App">
       <h1 className="center">Fiji Radio</h1>
       <div>
         <br></br>
-      <div className="text-center border">
-        <h1>
-          Radio Fiji 2
+        <div className="text-center border">
+          <h1>
+            Radio Fiji 2
       </h1>
-        <audio controls>
-          <source src="http://peridot.streamguys.com:7150/RFTwo" type="audio/mpeg"/>
-        </audio>
-      </div>
-      <br></br>
-      <div className="text-center border">
-        <h1>Radio navtarang fiji</h1>
-        <audio controls preload="auto">
-          <source src="https://stream.fijivillage.com/navtarang?ver=636650.mp3" type="audio/mp3"></source>
-        </audio>
-      </div>
-      <br></br>
-      <div className="text-center border">
-        <h1>Radio Sargam</h1>
-        <audio controls preload="auto">
-          <source src="https://stream.fijivillage.com/radiosargam" type="audio/mp3"></source>
-        </audio>
+          <audio controls>
+            <source src="http://peridot.streamguys.com:7150/RFTwo" type="audio/mpeg" />
+          </audio>
+        </div>
         <br></br>
-<button className="" onClick={() => window.location.reload(false)}>Click to reload!</button>
+        <div className="text-center border">
+          <h1>Radio navtarang fiji</h1>
+          <audio controls preload="auto">
+            <source src="https://stream.fijivillage.com/navtarang?ver=636650.mp3" type="audio/mp3"></source>
+          </audio>
+        </div>
+        <br></br>
+        <div className="text-center border">
+          <h1>Radio Sargam</h1>
+          <audio controls preload="auto">
+            <source src="https://stream.fijivillage.com/radiosargam" type="audio/mp3"></source>
+          </audio>
+          <br></br>
+        </div>
       </div>
+      <br></br>
+      <div className="text-center">
+        <button className="" onClick={refreshPage}>Click to reload!</button>
       </div>
-{/* <div className="text-center border">
+      {/* <div className="text-center border">
   <h1>Bula FM Fiji</h1>
   <audio controls>
     <source src="http://peridot.streamguys.com:7150/Bula" type="audio/mp3"></source>
@@ -48,10 +57,10 @@ function App() {
       <source src="http://peridot.streamguys.com:7150/Mirchi" type="audio/mp3"/>
       </audio>
       </div> */}
-      
+
       <footer className="colorFooter text-center">Made By Chidoro Batkau V1.0 MixKarDo</footer>
     </div>
-    
+
   );
 }
 
